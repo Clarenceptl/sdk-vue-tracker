e46d2d34115c07ed022f3c0f4f013345320a9513
Clacla <potel.clarence@gmail.com>
2023-07-12 00:08:14 +0200

    finish sdk

--- index.js
+++ index.js
@@ -1,37 +1,28 @@
 import FingerprintJS from "@fingerprintjs/fingerprintjs";
+import throttle from "lodash.throttle";
+import UAParser from "ua-parser-js";
+import { onMounted, onUnmounted, ref } from "vue";
 
-export const sendEvent = (data, type = "beacon") => {
-  const url = "http://localhost:3000/api/events";
-  if (type === "beacon") {
-    navigator.sendBeacon(url, {
-      method: "POST",
-      body: JSON.stringify(data),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-  } else {
-    fetch(url, {
-      method: "POST",
-      body: JSON.stringify(data),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-  }
+const sendEvent = async (data, path) => {
+  const url = `http://localhost:3080/v1/${path}`;
+
+  fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
 };
 
-export default async function initTracker(Vue, options) {
+export const useMousePosition = async (options) => {
   if (!options.APP_ID) {
     throw new Error("Please provide the APP_ID");
   }
 
-  const idVisitor = FingerprintJS.load()
-    .then((fp) => fp.get())
-    .then((result) => {
-      // This is the visitor identifier:
-      return result.visitorId;
-    });
+  const uaParser = new UAParser();
+  const idVisitor = (await (await FingerprintJS.load()).get()).visitorId;
+
   if (!idVisitor) {
     throw new Error("Error id visitor");
   }
@@ -39,33 +30,118 @@ export default async function initTracker(Vue, options) {
     APP_ID: options.APP_ID,
     service: options.service || "website",
     visitorId: idVisitor,
-    // id session d'analitycs timestamp
+    uaParser: uaParser.getResult(),
+    session: 1,
     // api endpoint
   };
+  const x = ref(0);
+  const y = ref(0);
+
+  const updateMousePosition = throttle((event) => {
+    x.value = event.clientX;
+    y.value = event.clientY;
+
+    sendEvent(
+      {
+        ...configData,
+        type: "mouse",
+        data: {
+          x: event.clientX,
+          y: event.clientY,
+        },
+      },
+      "event/front"
+    );
+  }, 100);
+
+  onMounted(() => {
+    window.addEventListener("mousemove", updateMousePosition);
+  });
+
+  onUnmounted(() => {
+    window.removeEventListener("mousemove", updateMousePosition);
+  });
+
+  return { x, y };
+};
+export const trackBackend = async (data) => {
+  const configData = {
+    APP_ID: data.APP_ID,
+    APP_SECRET: data.APP_SECRET,
+    service: data.service || "backend",
+  };
+  sendEvent(
+    {
+      ...configData,
+      type: "backend",
+      data,
+    },
+    "event/backend"
+  );
+};
+
+export default async function tracker(Vue, options, router) {
+  const inactivityTimeout = options.inactivityTimeout || 3000;
+
+  if (!options.APP_ID) {
+    throw new Error("Please provide the APP_ID");
+  }
+
+  const uaParser = new UAParser();
+  const idVisitor = (await (await FingerprintJS.load()).get()).visitorId;
+
+  if (!idVisitor) {
+    throw new Error("Error id visitor");
+  }
+  const configData = {
+    APP_ID: options.APP_ID,
+    service: options.service || "website",
+    visitorId: idVisitor,
+    uaParser: uaParser.getResult(),
+    session: 1,
+    // api endpoint
+  };
+
+  router.afterEach((to, from) => {
+    const { fullPath, meta, query } = to;
+    sendEvent(
+      {
+        ...configData,
+        type: "pageview",
+        data: {
+          fullPath,
+          meta,
+          query,
+        },
+      },
+      "event/front"
+    );
+  });
   const eventListeners = {};
-  console.log("configData", configData);
+
   Vue.directive("track", {
     mounted(el, binding) {
       eventListeners[binding.arg] = () => {
-        sendEvent({
-          ...configData,
-          event: binding.modifiers,
-          tag: binding.arg,
-          modifier: binding.modifiers,
-        });
-        console.log("binding", binding);
+        // resetInactivityTimer();
+        sendEvent(
+          {
+            ...configData,
+            type: "tag",
+            event: binding.modifiers,
+            tag: binding.arg,
+          },
+          "event/front"
+        );
       };
       for (const modifier in binding.modifiers) {
         el.addEventListener(modifier, eventListeners[binding.arg]);
       }
-      // el.addEventListener('click', eventListeners[binding.arg]);
     },
     unmounted(el, binding) {
       for (const modifier in binding.modifiers) {
         el.removeEventListener(modifier, eventListeners[binding.arg]);
       }
       delete eventListeners[binding.arg];
-      // el.removeEventListener('click', eventListeners[binding.arg]);
     },
   });
 }
